4e83a065b4248d08e660b87d40e275bb40db16fd
LightProger <photoluxe@mail.ru>
2016-12-26 22:22:25 +0600

    Java_lesson_Hello

---
+++ src/lesson_java_hello/Main.java
@@ -0,0 +1,16 @@
+package lesson_java_hello;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("*     *  *****  *      *      *****");
+        System.out.println("*     *  *      *      *      *   *");
+        System.out.println("*     *  *      *      *      *   *");
+        System.out.println("*******  *****  *      *      *   *");
+        System.out.println("*     *  *      *      *      *   *");
+        System.out.println("*     *  *      *      *      *   *");
+        System.out.println("*     *  *****  *****  *****  *****");
+
+
+    }
+}
